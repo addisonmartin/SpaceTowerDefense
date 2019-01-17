59b8508ac75ac19747c5f20f8fecf984b57317c5
Cullen Carns <cullenjcarns@gmail.com>
2019-01-17 10:38:12 -0800

    Initial Project creation
    Basic orbiting demo and project to work with

---
+++ SpaceTD/Assets/Scripts/Orbit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.position - transform.localPostion is equivalent to the position of the parent
+        transform.RotateAround(transform.position - transform.localPosition, Vector3.forward, speed * Time.deltaTime);
+    }
+}
