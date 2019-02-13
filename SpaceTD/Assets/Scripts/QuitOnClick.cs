c16b116508341688af12101643711fb90d9bbe1b
Addison Martin <addisonejmartin@gmail.com>
2019-02-12 17:34:16 -0800

    Finished bringing in main menu from Lab4

---
+++ SpaceTD/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Addison
+public class QuitOnClick : MonoBehaviour
+{
+
+   // Written by Addison
+   public void Quit()
+   {
+      #if UNITY_EDITOR
+         UnityEditor.EditorApplication.isPlaying = false;
+      #else
+         Application.Quit();
+      #endif
+   }
+}
