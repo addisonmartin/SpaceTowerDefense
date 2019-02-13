a13c62e55374e8f17c4fd7ddc66594644fdec138
Cullen Carns <cullenjcarns@gmail.com>
2019-02-13 15:23:55 -0800

    Final merge, a working prototype ish thing

--- SpaceTD/Assets/Scripts/Controllers/Enemy.cs
+++ SpaceTD/Assets/Scripts/Controllers/Enemy.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
     //Cullen
     public float speed;
     private GameObject target;
     private Vector2 d;
     private Rigidbody2D rb;
-    private int scrapValue = 12;
+    private int scrapValue = 3;
     private float hp = 100f;
+    private Vector2 dir;
 
-    private int scrapToEmit = 1;
+    private int scrapToEmit = 4;
     public GameObject scrapPrefab;
 
     //public GameObject EnemyDeath;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         //Cullen
         target = GameObject.FindGameObjectWithTag("Player");
         d = target.transform.position - transform.position;
@@ -29,10 +32,34 @@ public class Enemy : MonoBehaviour {
         transform.rotation = Quaternion.Euler(0f, 0f, zRot - 90);
     }
 
-    void Uppdate() {
-        //Cullen
-
-        //transform.position = new Vector3(transform.position.x + d.x * speed * Time.deltaTime, transform.position.y + d.y * speed * Time.deltaTime, transform.position.z);
+    void Update()
+    {
+        //Daniel
+        dir = new Vector2(-transform.right.y, transform.right.x);
+        rb.velocity = dir * speed;
+        /*bool boop = CheckPath();
+        if (boop)
+        {
+            if (goRight())
+            {
+                transform.Rotate(new Vector3(0, 0, -1));
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 0, 1));
+            }
+        }
+        else
+        {*/
+        d = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 1);
+        //}
+        if (Vector2.Distance(transform.position, target.transform.position) <= 5)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
     }
 
     public void takeDamage(float damage) {
@@ -46,7 +73,11 @@ public class Enemy : MonoBehaviour {
         //Cullen
         if (collision.gameObject.CompareTag("Player")) {
             Destroy(gameObject);
-        }
+        }/*else if (collision.gameObject.CompareTag("Projectile")) {
+            Destroy(collision.gameObject);
+            Explode();
+            //Destroy(gameObject);
+        }*/
     }
 
     //Lukas
--- SpaceTD/Assets/Scripts/Core/Level.cs
+++ SpaceTD/Assets/Scripts/Core/Level.cs
@@ -9,14 +9,20 @@ public abstract class Level : MonoBehaviour {
 
     public List<Enemy> possibleEnemies = new List<Enemy>();
     public List<Vector2> possibleEnemySpawnLocations = new List<Vector2>();
+    public List<int> waves = new List<int>();
     public float enemySpawnRate;
+    public float waveRate;
 
     private float timeToNextSpawn = 0;
+    private float waveTimer = 0;
+    private int waveNum;
 
     // Start is called before the first frame update
     // Written by Addison
     void Start() {
         SetupLevel();
+        waveNum = 0;
+        waveTimer = waveRate;
     }
 
     public virtual void SetupLevel() {
@@ -33,24 +39,37 @@ public abstract class Level : MonoBehaviour {
     // Written by Cullen.
     // Updated by Addison to work for any number of enemies and spawn locations.
     private void CheckEnemySpawn() {
-        if (timeToNextSpawn <= 0) {
-            int enemyIndex = Random.Range(0, possibleEnemies.Count);
-            Enemy e = Instantiate(possibleEnemies[enemyIndex], new Vector3(-20, -20, 0), Quaternion.identity);
-            Vector2 spawnLocation = possibleEnemySpawnLocations[Random.Range(0, possibleEnemySpawnLocations.Count)];
-            e.transform.position = Camera.allCameras[0].ViewportToWorldPoint(new Vector3(spawnLocation.x, spawnLocation.y));
-            e.transform.position = new Vector3(e.transform.position.x, e.transform.position.y);
-            timeToNextSpawn = enemySpawnRate;
+        if (waveNum >= waves.Count) {
+            return;
+            //waveNum %= waves.Count;
+        }
+        if (waveTimer <= 0) {
+            if (timeToNextSpawn <= 0) {
+                int enemyIndex = Random.Range(0, possibleEnemies.Count);
+                Enemy e = Instantiate(possibleEnemies[enemyIndex], new Vector3(-20, -20, 0), Quaternion.identity);
+                Vector2 spawnLocation = possibleEnemySpawnLocations[waveNum];
+                e.transform.position = Camera.allCameras[0].ViewportToWorldPoint(new Vector3(spawnLocation.x, spawnLocation.y));
+                e.transform.position = new Vector3(e.transform.position.x, e.transform.position.y); //set enemy z to 0
+                timeToNextSpawn = enemySpawnRate;
 
-            // The rate at which enemies spawn speeds up during the level.
-            if (enemySpawnRate > .5f) {
-                enemySpawnRate -= .05f;
+                // The rate at which enemies spawn speeds up during the level.
+                if (enemySpawnRate > .1f) {
+                    enemySpawnRate -= .05f;
+                } else {
+                    enemySpawnRate = .1f;
+                }
+                waves[waveNum]--;
+                if (waves[waveNum] == 0) {
+                    waveTimer = waveRate;
+                    waveNum++;
+                }
+
             } else {
-                enemySpawnRate = .5f;
+                timeToNextSpawn -= Time.deltaTime;
             }
         } else {
-            timeToNextSpawn -= Time.deltaTime;
+            waveTimer -= Time.deltaTime;
         }
-
     }
 
     // Written by Addison
