92ae3c9d218ee268066869de624e3ba7c16ae08f
Addison Martin <addisonejmartin@gmail.com>
2019-02-15 10:27:33 -0800

    You can now view the towers orbitting the selected astral body

--- SpaceTD/Assets/Scripts/Core/AstralBody.cs
+++ SpaceTD/Assets/Scripts/Core/AstralBody.cs
@@ -12,19 +12,80 @@ public class AstralBody : MonoBehaviour, ISelectable {
     private const int ORBITAL_1_MAX = 8;
     private List<Tower> orbital1 = new List<Tower>();
 
+    // Written by Addison
+    public GridLayoutGroup orbitalPanel;
+
+    public Image tower1Image;
+    public Image tower2Image;
+    public Text towerDetailsTextPrefab;
+
+    // Written by Cullen
     public void Start() {
-        selectedImage = GameObject.Find("SelectedTowerDisplay").GetComponent<Image>();
+        selectedImage = GameObject.Find("SelectedAstralBodyDisplay").GetComponent<Image>();
         //selectedImage = FindObjectOfType<Image>();
     }
 
     public void display() {
-        selectedImage.sprite = GetComponent<SpriteRenderer>().sprite;
+         // Written by Cullen
+         selectedImage.sprite = GetComponent<SpriteRenderer>().sprite;
+
+         // Written by Addison
+         foreach (Transform child in this.transform)
+         {
+            GameObject gameObject = child.gameObject;
+
+            bool isTower1 = (gameObject.tag == "Tower1");
+            bool isTower2 = (gameObject.tag == "Tower2");
+
+            if (isTower1 || isTower2)
+            {
+               Image towerImage;
+
+               if (isTower1)
+               {
+                  towerImage = Instantiate(tower1Image) as Image;
+               }
+               else
+               {
+                  towerImage = Instantiate(tower2Image) as Image;
+               }
+
+               towerImage.transform.SetParent(orbitalPanel.transform, false);
+               towerImage.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+               towerImage.transform.localPosition = Vector3.zero;
+
+               Text towerDetails;
+               towerDetails = Instantiate(towerDetailsTextPrefab) as Text;
+
+               if (isTower1)
+               {
+                  towerDetails.text = "Speed: 3, Range: 25\nDamage: 25, Cooldown: 1";
+               }
+               else
+               {
+                  towerDetails.text = "Speed: 3, Range: 40\nDamage: 100, Cooldown: 3";
+               }
+
+               towerDetails.transform.SetParent(orbitalPanel.transform, false);
+               towerDetails.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+               towerDetails.transform.localPosition = Vector3.zero;
+            }
+         }
     }
 
     public void undisplay() {
-        selectedImage.sprite = null;
+         // Written by Cullen
+         selectedImage.sprite = null;
+
+         // Wrriten by Addison
+         foreach (Transform child in orbitalPanel.transform)
+         {
+            GameObject gameObject = child.gameObject;
+            Destroy(gameObject);
+         }
     }
 
+    // Written by Cullen
     public bool addTower(int orbital, Tower t) {
         switch (orbital) {
             case 0:
@@ -33,6 +94,7 @@ public class AstralBody : MonoBehaviour, ISelectable {
                 }
                 t.setOrbital(0);
                 orbital0.Add(t);
+                display();
                 return true;
             case 1:
                 if (orbital1.Count >= ORBITAL_1_MAX) {
@@ -40,10 +102,12 @@ public class AstralBody : MonoBehaviour, ISelectable {
                 }
                 t.setOrbital(1);
                 orbital1.Add(t);
+                display();
                 return true;
             default:
                 return false;
         }
+
     }
 
 }
