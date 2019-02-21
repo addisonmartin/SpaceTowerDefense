25652efffead1cb6084a1fbd632a228c7254e496
DDeegan <38265528+DDeegan@users.noreply.github.com>
2019-02-21 10:12:11 -0800

    bluh

---
+++ SpaceTD/Assets/Scripts/Controllers/EnemyBullet.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{ //Lukas
+    private Rigidbody2D rb;
+
+    //Cullen
+    public float speed;
+    private static CameraController camControl;
+    private Vector2 d;
+    private float damage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camControl == null)
+        {
+            camControl = GameObject.Find("Camera Rig").GetComponent<CameraController>();
+        }
+    }
+
+    //Cullen
+    public void setDamage(float d)
+    {
+        damage = d;
+    }
+
+    private void Update()
+    {
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        //Cullen
+        if (collision.gameObject.CompareTag("Player")) {
+
+            target.GetComponent<Player>().takeDamage(damage);
+            Destroy(gameObject);
+        }/*else if (collision.gameObject.CompareTag("Projectile")) {
+            Destroy(collision.gameObject);
+            Explode();
+            //Destroy(gameObject);
+        }*/
+    }
+
+        //Cullen
+        transform.position = new Vector3(transform.position.x + d.x * speed * Time.deltaTime, transform.position.y + d.y * speed * Time.deltaTime, transform.position.z);
+        //Destroy projectile if it leaves the screen
+        if (!camControl.inWorld(transform.position))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Cullen
+    public void setDirection(Vector2 dir)
+    {
+        this.d = dir;
+        float zRot = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, zRot - 90f);
+        //rb.velocity = d * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Cullen
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            target.GetComponent<Player>().takeDamage(damage);
+            Destroy(gameObject);
+        }/*else if (collision.gameObject.CompareTag("Projectile")) {
+            Destroy(collision.gameObject);
+            Explode();
+            //Destroy(gameObject);
+        }*/
+    }
+}
