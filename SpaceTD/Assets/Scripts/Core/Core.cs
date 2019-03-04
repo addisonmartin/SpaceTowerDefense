44c40b0ee5e61b8324fce3ee061bde426d4e71f3
Daniel <DDDragoni1@gmail.com>
2019-03-04 01:22:22 -0800

    Fleshing out tutorial

--- SpaceTD/Assets/Scripts/Core/Core.cs
+++ SpaceTD/Assets/Scripts/Core/Core.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Core : MonoBehaviour {
     public static bool freeze;
+    public int levelNum;
+    public static int waveNum;
     // Start is called before the first frame update
     void Start() {
         freeze = false;
+        waveNum = 0;
     }
 
     // Update is called once per frame
@@ -14,5 +17,10 @@ public class Core : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.G)) {
             freeze = !freeze;
         }
+    }
+
+    public static void waveComplete(int j)
+    {
+        waveNum = j;
     }
 }
