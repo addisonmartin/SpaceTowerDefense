29de91af8658b41bee96841358cfd2175fb5b375
Cullen <cullenjcarns@gmail.com>
2019-03-17 19:28:53 -0700

    Fixed merges and level difficulties

--- SpaceTD/Assets/Scripts/Controllers/MenuFuncs.cs
+++ SpaceTD/Assets/Scripts/Controllers/MenuFuncs.cs
@@ -6,24 +6,24 @@ using UnityEngine.UI;
 
 public class MenuFuncs : MonoBehaviour {
 
-   // Written by Addison
-   public Text levelTitle;
-   public Button previousLevelButton;
-   public Button nextLevelButton;
-   public Image levelImage;
-   public Toggle endlessToggle;
-   public Dropdown difficultyDropdown;
-   public Button startButton;
-   public int levelNum;
-   public Text highscoreText;
-   public bool isLevelSelect;
-   public GameObject pauseMenu;
+    // Written by Addison
+    public Text levelTitle;
+    public Button previousLevelButton;
+    public Button nextLevelButton;
+    public Image levelImage;
+    public Toggle endlessToggle;
+    public Dropdown difficultyDropdown;
+    public Button startButton;
+    public int levelNum;
+    public Text highscoreText;
+    public bool isLevelSelect;
+    public GameObject pauseMenu;
 
     public void Start() {
         Cursor.visible = true;
 
         if (isLevelSelect) {
-           lockUnlockLevels();
+            lockUnlockLevels();
         }
     }
 
@@ -38,15 +38,10 @@ public class MenuFuncs : MonoBehaviour {
 
     // Written by Addison
     public void LoadSceneByIndex(int sceneIndex) {
-      if (isLevelSelect) {
-        Core.endlessMode = endlessToggle.isOn;
-        if (difficultyDropdown.itemText.text == "Haungs") {
-           Core.isHaungsMode = true;
+        if (isLevelSelect) {
+            Core.endlessMode = endlessToggle.isOn;
+            Core.difficulty = difficultyDropdown.value;
         }
-        else {
-           Core.isHaungsMode = false;
-        }
-     }
         SceneManager.LoadScene(sceneIndex);
     }
 
@@ -55,42 +50,37 @@ public class MenuFuncs : MonoBehaviour {
     }
 
     public void resumeGame() {
-      Core.freeze = false;
-      pauseMenu.SetActive(false);
-   }
+        Core.freeze = false;
+        pauseMenu.SetActive(false);
+    }
 
     public void lockUnlockLevels() {
 
-      if (levelNum == 1) {
-         startButton.interactable = Core.levelOneUnlocked;
-         endlessToggle.interactable = Core.levelOneCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelOneEndlessModeHighScore;
-      }
-      else if (levelNum == 2) {
-         startButton.interactable = Core.levelTwoUnlocked;
-         endlessToggle.interactable = Core.levelTwoCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelTwoEndlessModeHighScore;
-      }
-      else if (levelNum == 3) {
-         startButton.interactable = Core.levelThreeUnlocked;
-         endlessToggle.interactable = Core.levelThreeCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelThreeEndlessModeHighScore;
-      }
-      else if (levelNum == 4) {
-         startButton.interactable = Core.levelFourUnlocked;
-         endlessToggle.interactable = Core.levelFourCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelFourEndlessModeHighScore;
-      }
-      else if (levelNum == 5) {
-         startButton.interactable = Core.levelFiveUnlocked;
-         endlessToggle.interactable = Core.levelFiveCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelFiveEndlessModeHighScore;
-      }
-      else if (levelNum == 6) {
-         startButton.interactable = Core.levelSixUnlocked;
-         endlessToggle.interactable = Core.levelSixCompleted;
-         highscoreText.text = "Endless Highscore: " + Core.levelSixEndlessModeHighScore;
-      }
-   }
+        if (levelNum == 1) {
+            startButton.interactable = Core.levelOneUnlocked;
+            endlessToggle.interactable = Core.levelOneCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelOneEndlessModeHighScore;
+        } else if (levelNum == 2) {
+            startButton.interactable = Core.levelTwoUnlocked;
+            endlessToggle.interactable = Core.levelTwoCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelTwoEndlessModeHighScore;
+        } else if (levelNum == 3) {
+            startButton.interactable = Core.levelThreeUnlocked;
+            endlessToggle.interactable = Core.levelThreeCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelThreeEndlessModeHighScore;
+        } else if (levelNum == 4) {
+            startButton.interactable = Core.levelFourUnlocked;
+            endlessToggle.interactable = Core.levelFourCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelFourEndlessModeHighScore;
+        } else if (levelNum == 5) {
+            startButton.interactable = Core.levelFiveUnlocked;
+            endlessToggle.interactable = Core.levelFiveCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelFiveEndlessModeHighScore;
+        } else if (levelNum == 6) {
+            startButton.interactable = Core.levelSixUnlocked;
+            endlessToggle.interactable = Core.levelSixCompleted;
+            highscoreText.text = "Endless Highscore: " + Core.levelSixEndlessModeHighScore;
+        }
+    }
 
 }
